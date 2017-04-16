b9215d79bbf5cccc49540ab5b6eadfa8d75164ba
Iris-W <zixin_wang@student.uml.edu>
2017-04-16 11:02:18 -0400

    Update fp-2048.rkt

--- fp-2048.rkt
+++ fp-2048.rkt
@@ -7,5 +7,12 @@
 
 (define (init-board n)
   (make-list n (make-list n 0)))
+  
+(define (choice ls)
+  (if (list? ls) (list-ref ls (random (length ls)))
+    (vector-ref ls (random (vector-length ls)))))
+    
+(define (get-piece)
+    (choice rand-dice))
 
 (define board (init-board 4)) ;;make an empty board 4x4
